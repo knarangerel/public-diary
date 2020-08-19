8bed03460b1e01961784ba009b4bab8104acae9c
knarangerel <khaliun.narangerel@gmail.com>
2020-08-19 17:14:44 -0400

    Add frontend urls

--- source/settings.py
+++ source/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'diary.apps.DiaryConfig',
-    'rest_framework'
+    'rest_framework',
+    'frontend'
 ]
 
 # Enable in production
