098f0d9647efa85dc7d6b11304ceeb869830c8d2
knarangerel <khaliun.narangerel@gmail.com>
2020-08-24 14:56:32 -0400

    Add functionality to see the most recent entry

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,99 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { render } from "react-dom";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      diary: {},
-      loaded: false,
-      placeholder: "Loading"
-    };
-  }
-
-  componentDidMount() {
-    fetch("today/")
-      .then(response => {
-        if (response.status > 400) {
-          return this.setState(() => {
-            return { placeholder: "Something went wrong!" };
-          });
-        }
-        return response.json();
-      })
-      .then(data => {
-        this.setState(() => {
-          return {
-            diary: data[0],
-            loaded: true
-          };
-        });
-        console.log(data[0].descriptors)
-      });
-  }
-
-  render() {
-    return (
-      <div>
-      <h3>{this.state.diary.date} - {this.state.diary.caption} - {this.state.diary.adjectives}</h3>
-      </div>
-    );
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: 20,
+  },
+  entryContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  innerContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    padding: 10,
+  },
+  infoText: {
+    fontFamily: 'sans-serif',
+    fontSize: 20,
+    margin: 50,
+  },
+  text: {
+    fontFamily: 'sans-serif',
+    fontSize: 18,
+    padding: 15,
+    margin: 0,
+  },
+  adjText: {
+    fontFamily: 'sans-serif',
+    fontSize: 18,
+    padding: 5,
+    paddingLeft: 15,
+    paddingTop: 15,
+    margin: 0,
   }
 }
 
+function App() {
+  const [diary, setDiary] = useState({
+    date: '',
+    caption: '',
+    rating: 0,
+    adjectives: []
+  })
+  const [checkedIn, setCheckedIn] = useState(true)
+
+  async function fetchDiary() {
+    try {
+      let response = await fetch('today/')
+      if (response.status > 400) {
+        setCheckedIn(false)
+        response = await fetch('lastavailable/')
+      }
+      const json = await response.json()
+      setDiary({...json[0]})
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => { fetchDiary() }, [])
+
+  return (
+  <div style={styles.container}>
+    {checkedIn ? (<p style={styles.infoText}>Thank you for checking in with me!</p>)
+               : (<p style={styles.infoText}>I have not checked in today. Please check back later!</p>)}
+    <div style={styles.entryContainer}>
+      <div style={styles.innerContainer}>
+        {checkedIn ? (<p style={styles.text}>What is the date today? </p>)
+                   : (<p style={styles.text}>The most recent entry is for: </p>)}
+        {checkedIn ? (<p style={styles.text}>How is my day going? </p>)
+                   : (<p style={styles.text}>Caption of the day: </p>)}
+        {checkedIn ? (<p style={styles.text}>If I were to rate my day: </p>)
+                   : (<p style={styles.text}>Rating of the day: </p>)}
+        {checkedIn ? (<p style={styles.text}>How am I feeling? </p>)
+                   : (<p style={styles.text}>Adjectives of the day:</p>)}
+      </div>
+      <div style={styles.innerContainer}>
+        <p style={styles.text}>{diary.date}</p>
+        <p style={styles.text}>{diary.caption}</p>
+        <p style={styles.text}>{diary.rating}/10</p>
+        {/* <div style={{height: 30, backgroundColor: 'yellow'}}/> */}
+        {diary.adjectives.map(adj => <p key={adj} style={styles.adjText}>{adj}</p>)}
+      </div>
+    </div>
+  </div>
+  );
+}
+
 export default App;
+
 
 const container = document.getElementById("app");
 render(<App />, container);
