c4400e801a1cf0d0c344d056c0b080e8f130b753
knarangerel <khaliun.narangerel@gmail.com>
2020-08-24 17:13:27 -0400

    Refactor styling elements

--- diary/urls.py
+++ diary/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('today/', views.DiaryView.as_view()),
+    path('lastavailable/', views.PastDiaryView.as_view()),
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,9 +1,29 @@
-from datetime import date
+import datetime
+from django.http import Http404
 from rest_framework import generics
 from .models import Diary
 from .serializers import DiarySerializer
 
 
 class DiaryView(generics.ListCreateAPIView):
-    queryset = Diary.objects.filter(date=date.today())
     serializer_class = DiarySerializer
+
+    def get_queryset(self):
+        diary = Diary.objects.filter(date=datetime.date.today())
+        if not diary:
+            raise Http404
+        return diary
+
+
+class PastDiaryView(generics.ListCreateAPIView):
+    serializer_class = DiarySerializer
+
+    def get_queryset(self):
+        num_of_days = 1
+        diary = Diary.objects.filter(
+            date=datetime.date.today() - datetime.timedelta(days=num_of_days))
+        while not diary:
+            num_of_days += 1
+            diary = Diary.objects.filter(
+                date=datetime.date.today() - datetime.timedelta(days=num_of_days))
+        return diary
